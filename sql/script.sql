f33d92b345a6346257ce176c4713744f857ba846
woosupyun <yunwoosup@naver.com>
2020-08-04 17:29:41 +0900

    add sql script파일

---
+++ sql/script.sql
@@ -0,0 +1,40 @@
+create database h_project;
+use h_project;
+
+create table User(
+	userId varchar(255) primary key,
+    userEmail varchar(255),
+    userName varchar(255),
+    userPassword varchar(255),
+    regDate	dateTime default now()
+);
+
+desc User;
+select * from User;
+
+create table Board(
+	board_no int unsigned,
+    title varchar(255),
+    content varchar(255),
+    imageName varchar(255),
+    regdate datetime default now(),
+    userName varchar(255),
+    userId varchar(255), 
+	primary key (board_no),
+    Foreign key(userId) references User(userId)
+);
+
+desc Board;
+select * from Board;
+
+create table Comment(
+	reply_no int unsigned,
+    Board_no int unsigned,
+    userId varchar(255),
+    content varchar(255),
+    primary key (reply_no),
+    Foreign key(board_no) references Board(board_no)
+);
+
+desc Comment;
+select * from Comment;
