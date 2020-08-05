442a29f370955bb564d333550c2082b54cfe548c
woosupyun <yunwoosup@naver.com>
2020-08-05 16:22:12 +0900

    add File Upload

--- src/board/write/controller/WriterBoardController.java
+++ src/board/write/controller/WriterBoardController.java
@@ -9,51 +9,60 @@ import javax.servlet.http.Part;
 
 import board.write.model.WriteRequest;
 import board.write.service.WriteArticleService;
+import board.write.service.WriteFileService;
 import controller.Controller;
 import user.model.User;
 
-public class WriterBoardController implements Controller{
-	private final static String VIEW_CODE= "/WEB-INF/view/list/write/writeArticle.jsp";
+public class WriterBoardController implements Controller {
+	private final static String VIEW_CODE = "/WEB-INF/view/list/write/writeArticle.jsp";
 	private WriteArticleService writeService = new WriteArticleService();
-	
+	private WriteFileService writeFile = new WriteFileService();
+
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		if(request.getMethod().equalsIgnoreCase("GET")) {
-			return processGet(request,response);
-		}else if(request.getMethod().equalsIgnoreCase("POST")) {
+		if (request.getMethod().equalsIgnoreCase("GET")) {
+			return processGet(request, response);
+		} else if (request.getMethod().equalsIgnoreCase("POST")) {
 			return processPost(request, response);
-		}else {
-		 	response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+		} else {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
 			return null;
 		}
 	}
+
 	private String processGet(HttpServletRequest request, HttpServletResponse response) {
 		return VIEW_CODE;
 	}
-	
-	
+
 	private String processPost(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+
 		Part filePart = request.getPart("fileName");
 		String fileName = filePart.getSubmittedFileName();
-		
+
 		fileName = fileName == null ? "" : fileName;
-		
+
 		Map<String, Boolean> errors = new HashMap<>();
 		request.setAttribute("errors", errors);
-		
+
 		User user = (User) request.getSession(false).getAttribute("userInfo");
 		WriteRequest WriteReq = createWriteRequest(request, fileName);
+		WriteReq.validate(errors);
+
+		if (!errors.isEmpty()) {
+			return VIEW_CODE;
+		}
+
 		writeService.write(WriteReq, user);
 		
+		if (!(fileName == null || fileName.isEmpty() || filePart.getSize() == 0)) {
+			writeFile.write(filePart);
+		}
+
 		return "/WEB-INF/view/list/total/totalList.jsp";
 	}
-	
+
 	private WriteRequest createWriteRequest(HttpServletRequest req, String fileName) {
 
-		return new WriteRequest(
-						req.getParameter("title"),
-						req.getParameter("content"),
-						fileName);
+		return new WriteRequest(req.getParameter("title"), req.getParameter("content"), fileName);
 	}
 }
---
+++ src/board/write/service/WriteFileService.java
@@ -0,0 +1,24 @@
+package board.write.service;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.http.Part;
+
+public class WriteFileService {
+
+	public void write(Part part) {
+		String path = "C:/Users/admin/git/H_project/WebContent/WEB-INF/image";
+		File file = new File(path);
+		
+		if (!file.exists() ) {
+			file.mkdirs();
+		}
+		
+		try {
+			part.write(path + "/" + part.getSubmittedFileName());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
